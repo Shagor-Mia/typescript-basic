a724f3476e1c4cfdbce0408351e47256a36b9adb
Shagor-Mia <infinityshagor18557@gmail.com>
2025-06-23 11:58:52 +0600

    second commit

--- index.ts
+++ index.ts
@@ -85,3 +85,108 @@ const userInfo = (): { Name: string; Age: number; Gender: string } => {
 console.log(userInfo());
 
 // type aliases
+type Persons = {
+  Name: string;
+  age?: number;
+  Location: string;
+};
+
+const User = (user: Persons) => {
+  return `Name is :${user.Name} ,Age :${user.age} and Location :${user.Location}`;
+};
+console.log(User({ Name: "sagor", age: 20, Location: "dhaka" }));
+
+// Intersection type
+type UserInfo = {
+  Name: string;
+  Age: number;
+};
+type Employee = {
+  Email: string;
+  Password: string;
+};
+
+type User = UserInfo & Employee;
+
+const user: User = {
+  Name: "Ali",
+  Age: 44,
+  Email: "ali@gmail.com",
+  Password: "3435re",
+};
+console.log(`Name:${user.Name}`);
+
+// Union
+type UserInfos = {
+  names: string;
+  ages: number;
+  genders: string;
+};
+
+type UserDetails = {
+  email: string;
+  password: string;
+};
+
+let users: UserInfos | UserDetails = {
+  names: "hamid",
+  ages: 23,
+  genders: "Male",
+};
+
+console.log(`${users.ages}`);
+
+// letral types
+let colors: "red" | "yellow";
+let color: "red" = "red";
+colors = "red";
+// colors = "green"; not allowed
+console.log(colors);
+
+// Tuples
+const game: [number, string, boolean] = [1, "cricket", true];
+console.log(game[2]);
+
+// Enum Type
+enum Clown {
+  funny = "funny",
+  joke = "joker",
+}
+console.log(Clown.joke);
+
+// OOP
+class Persone {
+  readonly name: string;
+  private email: string;
+  protected age: number;
+
+  constructor(name: string, age: number, email: string) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+  public getMail(): string {
+    return this.email;
+  }
+}
+const perso = new Persone("ali", 30, "fjd@.com");
+console.log(perso);
+
+class Per extends Persone {
+  address: string;
+  location: string;
+  constructor(
+    address: string,
+    name: string,
+    email: string,
+    age: number,
+    location: string
+  ) {
+    super(name, age, email);
+    this.address = address;
+    this.location = location;
+  }
+}
+
+const P1 = new Per("dhaka", "ali", "jdhfjdf@g.com", 34, "Bangladesh");
+console.log(P1);
